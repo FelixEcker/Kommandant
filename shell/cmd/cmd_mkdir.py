9957b4d5fb9a35fc4bf78ac231aa8b7cae46cd1c
SoFa <felix0eckert0.online.de@gmail.com>
2018-08-09 18:13:10 +0200

    Fixed issue with the mkdir command and added modes

--- shell/cmd/cmd_mkdir.py
+++ shell/cmd/cmd_mkdir.py
@@ -6,7 +6,22 @@ def exec(cmd):
 	try:
 		path = cmd[1]
 		try:
-			mkdir(path, 755 )
+			try:
+				perms = int(cmd[2])
+				try:
+					mkdir(path, perms)
+					return
+				except FileExistsError:
+					print("{}Error: mkdir: File / Directory already exists".format(Fore.LIGHTRED_EX))
+					return
+			except (ValueError, IndexError) as ex:
+				if type(ex).__name__ == "ValueError":
+					print("{}Error: mkdir: 2nd argument needs to be an integer not a string".format(Fore.LIGHTRED_EX))
+					return
+				else:
+					mkdir(path)
+					return
+
 		except FileExistsError:
 			print("{}Error: mkdir: File / Directory already exists".format(Fore.LIGHTRED_EX))
 			return
@@ -14,5 +29,5 @@ def exec(cmd):
 
 		print("Directory was created Succesfully")
 	except IndexError:
-		print("{}Error: mkdir: mkdir requires 1 argument but 0 were given".format(Fore.LIGHTRED_EX))
+		print("{}Error: mkdir: mkdir requires atleast 1 argument but 0 were given".format(Fore.LIGHTRED_EX))
 		return
