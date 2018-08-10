35d23d7e8773dcd0f6a422cc668e91e8b77543f7
SoFa <felix0eckert0.online.de@gmail.com>
2018-08-10 07:25:51 +0200

    Fixed issues with python and python3
    Path can now contain spaces

--- shell/cmd/cmd_python.py
+++ shell/cmd/cmd_python.py
@@ -1,19 +1,58 @@
 from os import system as sys
+from itertools import islice
+from pathlib import Path
+from .. import utils as ut
 from colorama import init, Fore
 init()
 
 def exec(cmd):
+	home = str(Path.home())
 	args = ""
+	bashPath = ""
+	winPath = ''
+	indx = 0
 
+	
 	try:
-		for Arg in cmd:
+		for index, Arg in enumerate(cmd):
 			if not Arg == "python":
-				args = args + Arg + " "
-		
+				bashPath = bashPath + "\\ " + Arg
+				print(bashPath)
+			if ".py" in Arg:
+				indx = index
+				break
+
+		bashPath = bashPath[2:]
+
+
+		for index, Arg in enumerate(cmd):
+			if not Arg == "python":
+				winPath = winPath + " " + Arg
+
+			if ".py" in Arg:
+				indx = index
+				break
+
+		winPath = winPath.lstrip()
+		winPath = '"' + winPath + '"'
+
+		for Arg in islice(cmd, indx, None):
+			if not Arg == ".py":
+				args = args + " " + Arg
+
+		args = args.lstrip()
+
+		if winPath.startswith("~"):
+			winPath = winPath.replace("~", home)
+
+		if bashPath.startswith("~"):
+			bashPath = bashPath.replace("~", home)
+
+
 		try:
 			sys("python {}".format(args))
 		except OSError:
-			print("{}Error: python: An unknown Exception occured whilst executing command!".format(Fore.LIGHTRED_EX))
+			print("python {0} {1}".format(bashPath, args) if ut.osname() == "OS X" or ut.osname() == "Linux" else "python {0} {1}".format(winPath, args))
 	except IndexError:
-		print ("executing python3 interpreter!")
-		sys("python3")
+		print ("executing python interpreter!")
+		sys("python")
--- shell/cmd/cmd_python3.py
+++ shell/cmd/cmd_python3.py
@@ -1,17 +1,56 @@
 from os import system as sys
+from itertools import islice
+from pathlib import Path
+from .. import utils as ut
 from colorama import init, Fore
 init()
 
 def exec(cmd):
+	home = str(Path.home())
 	args = ""
+	bashPath = ""
+	winPath = ''
+	indx = 0
 
 	try:
-		for Arg in cmd:
+		for index, Arg in enumerate(cmd):
 			if not Arg == "python3":
-				args = args + Arg + " "
-		
+				bashPath = bashPath + "\\ " + Arg
+				print(bashPath)
+			if ".py" in Arg:
+				indx = index
+				break
+
+		bashPath = bashPath[2:]
+		print(bashPath)
+
+
+		for index, Arg in enumerate(cmd):
+			if not Arg == "python3":
+				winPath = winPath + " " + Arg
+
+			if ".py" in Arg:
+				indx = index
+				break
+
+		winPath = winPath.lstrip()
+		winPath = '"' + winPath + '"'
+
+		for Arg in islice(cmd, indx, None):
+			if not Arg == ".py":
+				args = args + " " + Arg
+
+		args = args.lstrip()
+
+		if winPath.startswith("~"):
+			winPath = winPath.replace("~", home)
+
+		if bashPath.startswith("~"):
+			bashPath = bashPath.replace("~", home)
+
+
 		try:
-			sys("python3 {}".format(args))
+			sys("python3 {0} {1}".format(bashPath, args) if ut.osname() == "OS X" or ut.osname() == "Linux" else "python3 {0} {1}".format(winPath, args))
 		except Exception:
 			print ("{}Error: An unknown error occured whilst executing script!".format(Fore.LIGHTRED_EX))
 	except IndexError:
