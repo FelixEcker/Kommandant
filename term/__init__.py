fee2e6fd89e79a0ce88115a68a673ca4b022e07b
SoFa <felix0eckert0.online.de@gmail.com>
2018-08-10 08:22:04 +0200

    Fixed input foreground color

--- term/__init__.py
+++ term/__init__.py
@@ -5,4 +5,4 @@ from os import getcwd as cwd
 
 class Functions:
     def prompt(usrname):
-        return input("{0}{1}@Kommandant:{2}{3} -${4} ".format(Fore.LIGHTBLUE_EX, usrname, Fore.GREEN, cwd(), Fore.RESET))
+        return input("{0}{1}@Kommandant:{2}{3} -${4} ".format(Fore.LIGHTBLUE_EX, usrname, Fore.GREEN, cwd(), Fore.WHITE))
